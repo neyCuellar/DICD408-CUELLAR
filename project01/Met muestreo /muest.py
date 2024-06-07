88288bdd0e3ef0990c29bd252e79bca064112200
neyCuellar <neydacz032240@gmail.com>
2024-06-07 23:45:59 +0000

    pr

---
+++ project01/Met muestreo /muest.py
@@ -0,0 +1,5 @@
+#Metodos de muestreo 
+##objetivo: muestrear una distribucion probabilistiica p(x) que es proporcional a f(x) pero que es dificil obtener la constante .
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
